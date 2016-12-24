7bf3cc0b258a30bc10d6067a13adef68295294ce
kamaubrian05 <kamaubrian05@gmail.com>
2016-12-24 22:15:51 +0300

    php

--- file.php
+++ file.php
@@ -1,16 +1,13 @@
 <?php
+  $name = $_GET['message1'];
+   $my_file= fopen("text.txt","a") or die ("Unable to open file");
+   fprintf($my_file,"$name");
 
-    $myfile = fopen("message.txt","w") or die ("Unable to open file!!");
-     //echo fread($myfile,filesize("message.txt"));
-     fprintf($myfile,"This is it");
-    fclose($myfile);
+
+
+
 
 
 ?>
 
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 12/11/16
- * Time: 4:51 PM
- */
+
